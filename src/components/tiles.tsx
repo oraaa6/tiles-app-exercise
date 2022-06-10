76fa007a888b2669ae9306414de38330ab698906
oraaa6 <oladra6@gmail.com>
2022-06-10 16:12:05 +0200

    add new class to tile and types to state

--- src/components/tiles.tsx
+++ src/components/tiles.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../styles/tiles.scss";
 
-type UsersDataTypes = {
+type UsersData = {
   group_id: number;
   cost: number;
   id: number;
@@ -10,36 +10,37 @@ type UsersDataTypes = {
   lifetime: number;
 };
 
+type UsersResponse = UsersData[];
 const Tiles: React.FC = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<UsersResponse>([]);
 
   useEffect(() => {
-    axios.get("http://localhost:4000/").then((response) => {
-      let newUsers = response.data;
+    axios.get<UsersResponse>("http://localhost:4000/").then((response) => {
+      const newUsers = response.data;
       setUsers(newUsers);
     });
   }, []);
-  const elements: JSX.Element[] = users.map((user: UsersDataTypes) => {
-    return (
-      <div className="tile" key={user.id}>
-        <p className="tile__keyword">{user.keyword}</p>
-        <div className="data-container tile__data-container">
-          <div className="data">
-            <p className="data__keyword">{user.id}</p>
-            <p className="data__keyword">{user.lifetime}</p>
-          </div>
-          <div className="data">
-            <p className="data__keyword">{user.cost}</p>
-            <p className="data__keyword">Expenses</p>
-          </div>
-        </div>
-      </div>
-    );
-  });
+
   return (
-    <>
-      <div className="tiles-container">{elements}</div>
-    </>
+    <div className="tiles-container">
+      {users.map((user, index) => {
+        return (
+          <div className={`tile tile-${index}`} key={user.id}>
+            <p className="tile__keyword">{user.keyword}</p>
+            <div className="data-container tile__data-container">
+              <div className="data">
+                <p className="data__keyword">{user.id}</p>
+                <p className="data__keyword">{user.lifetime}</p>
+              </div>
+              <div className="data">
+                <p className="data__keyword">{user.cost}</p>
+                <p className="data__keyword">Expenses</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
